a4656f64aa73772120ba94b3cc36db7388816c84
Ricardo S. Chao <chaoannricardo@protonmail.com>
2021-04-15 13:57:25 +0800

    [minor]

--- scripts/Word2Vec/Word2Vec_Gensim_PretrainedModel.py
+++ scripts/Word2Vec/Word2Vec_Gensim_PretrainedModel.py
@@ -7,6 +7,9 @@ from gensim.models.word2vec import Word2Vec
 import gensim.downloader as api
 
 if __name__ == '__main__':
+    print("Loading model...")
     model = api.load("conceptnet-numberbatch-17-06-300")
-    model.predict_output_word(["Obama", "Hawaii"], topn=10)
-
+    # model = api.load("glove-twitter-25")
+    print("Model loaded.")
+    print(model.most_similar("cat"))
+    print(model.predict_output_word(["Obama", "Hawaii"], topn=10))
