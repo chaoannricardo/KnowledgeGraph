505a8c34a001b0f4ad14e17837276dfb96b6db42
Ricardo S. Chao <chaoannricardo@protonmail.com>
2021-05-19 20:18:46 +0800

    [up] bootstrapping filtering script

--- scripts/Bootstrapping/FilterSeedRelation.py
+++ scripts/Bootstrapping/FilterSeedRelation.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf8 -*-
 import codecs
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -7,12 +8,17 @@ if __name__ == '__main__':
     ''' Configurations '''
     SEED_IMPORT_PATH = "../../../KnowledgeGraph_materials/data_kg/baiduDatasetTranditional_Cleansed/SEED_RELATION_BASIC.csv"
     FILTER_SEED_EXPORT_PATH = "../../../KnowledgeGraph_materials/data_kg/baiduDatasetTranditional_Cleansed/SEED_RELATION_BASIC_FILTER.csv"
-    FILTER_COUNT_CRITEREA = 13
+    FILTER_COUNT_CRITEREA = 20
+    IF_SHOW_ESTIMATE_CURVE = True
+    STARTING_FILTER = 5
     ''' Process Starts '''
     seed_pattern_list = []
 
     seed_basic = codecs.open(SEED_IMPORT_PATH, mode="r", encoding="utf8", errors="ignore")
     seed_filter_export = codecs.open(FILTER_SEED_EXPORT_PATH, mode="w", encoding="utf8", errors="ignore")
+    starting_filter = STARTING_FILTER if IF_SHOW_ESTIMATE_CURVE else FILTER_COUNT_CRITEREA
+    filter_num_list = []
+    filter_seed_count_list = []
 
     for seedIndex, seedElement in enumerate(seed_basic.readlines()):
         original_pattern = seedElement.split("&")[0]
@@ -20,18 +26,46 @@ if __name__ == '__main__':
         original_pattern = original_pattern.replace(relation.replace("\n", ""), "Predicate")
         seed_pattern_list.append(original_pattern)
 
-    # calculate and filter out import seeds
-    data_calculate = pd.DataFrame({
-        "SeedType": seed_pattern_list
-    })
+    for iteration in range(starting_filter, FILTER_COUNT_CRITEREA):
+        # calculate and filter out import seeds
+        data_calculate = pd.DataFrame({
+            "SeedType": seed_pattern_list
+        })
 
-    data_filter = data_calculate["SeedType"].value_counts().reset_index()
-    data_filter = data_filter[data_filter["SeedType"] >= FILTER_COUNT_CRITEREA].iloc[:, 0].tolist()
+        data_filter = data_calculate["SeedType"].value_counts().reset_index()
+        data_filter = data_filter[data_filter["SeedType"] >= iteration].iloc[:, 0].tolist()
+        filter_num_list.append(iteration)
+        filter_seed_count_list.append(len(data_filter))
 
     for lineIndex, lineElement in enumerate(data_filter):
         seed_filter_export.write(lineElement + "\n")
 
-    # print(len(data_filter[data_filter["SeedType"] >= 5]))
+    print(filter_num_list)
+    print(filter_seed_count_list)
+
+    if IF_SHOW_ESTIMATE_CURVE:
+        plt.bar(filter_num_list, filter_seed_count_list)
+        plt.xlabel("Filter Threshold")
+        plt.xticks(filter_num_list)
+        plt.ylabel("Unique Seed Types")
+        plt.title("Bootstrapping Seed Filtering")
+
+        # https://www.python-graph-gallery.com/10-barplot-with-number-of-observation
+        for i in range(len(filter_seed_count_list)):
+            plt.text(x=filter_num_list[i]-0.2, y=filter_seed_count_list[i] + 0.5,
+                     s=filter_seed_count_list[i], size=10, c="navy",
+                     fontweight="bold")
+
+
+        plt.show()
+
+
+
+
+
+
+
+
 
 
 
